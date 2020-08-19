11898d134d5cb59389652e0636d6f83e2754ce1d
ierrandonea <ierrandoneag@gmail.com>
2020-08-19 17:13:08 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from 'react';
 
 function App() {
+  const [state, setState] = useState(["Make the bed", "Wash my hands", "Eat", "Walk the dog"])
+
+  const inputTask = useRef('');
+
+  const handleKeyPress = e => {
+   if (e.keyCode === 13 && e.target.value !== '') {
+      setState([...state, e.target.value]);
+      e.target.value = '';
+    } else {
+       console.log(inputTask.current.value);
+    }
+  }
+
+  const handleClick = index => {
+      state.splice(index, 1);
+      console.log(state);
+      setState([...state])
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="card d-flex justify-content-center
+    align-items-center text-center">
+      <h3>To do's</h3>
+      <input type="text" ref={inputTask} onKeyDown={handleKeyPress} />
+      <ul className="list-item-group">
+        {
+        state.map((task,index) => {
+          return (
+          <li className="list-item" key={index} onClick={() => handleClick(index)}>{task}</li>
+          )
+        })
+        }
+      </ul>
+      <span>{state.length} items left</span>
     </div>
   );
 }

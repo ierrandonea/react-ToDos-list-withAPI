2deb747a44221944886741ac7852d4c989b70d38
ierrandonea <ierrandoneag@gmail.com>
2020-08-20 11:29:10 -0400

    updated styles

--- src/App.js
+++ src/App.js
@@ -16,9 +16,10 @@ function App() {
 
   const handleClick = index => {
     state.splice(index, 1);
-    console.log(state);
     setState([...state])
   };
+
+  const [mHover, setMHover] = useState(false)
 
   return (
     <div className="card d-flex justify-content-center
@@ -26,16 +27,26 @@ function App() {
       <div className="row w-100 no-gutters">
         <div className="col-12">
           <h3 className="text-center mt-2">To do's</h3>
-          <input className="form-control rounded-0 border-left-0 border-right-0 border-top-0" type="text" ref={inputTask} onKeyDown={handleKeyPress} placeholder="New tasks goes here..."/>
+          <input className="form-control rounded-0 border-left-0 border-right-0 border-top-0" type="text" ref={inputTask} onKeyDown={handleKeyPress} placeholder="New tasks goes here..." />
           <ul className="list-group rounded-0 border-0">
             {
               state.length > 0 ?
-              state.map((task, index) => {
-                return (
-                  <li className="list-group-item d-flex justify-content-between align-items-baseline rounded-0 border-left-0 border-right-0 border-top-0" key={index} onClick={() => handleClick(index)}>{task}<i class="fas fa-check-square"></i></li>
-                )
-              }) :
-              <li className="list-group-item text-muted rounded-0 border-left-0 border-right-0 border-top-0">Empty list</li>
+                state.map((task, index) => {
+                  return (
+                    <li
+                      className="list-group-item d-flex justify-content-between align-items-baseline rounded-0 border-left-0 border-right-0 border-top-0"
+                      key={index}
+                      onClick={() => handleClick(index)}>
+                      {task}
+                      <i
+                        class={"fas" + (mHover === true ? " fa-minus-square" : " fa-check-square")}
+                        onMouseOver={() => { setMHover(true) }}
+                        onMouseOut={() => { setMHover(false) }}>
+                      </i>
+                    </li>
+                  )
+                }) :
+                <li className="list-group-item text-muted rounded-0 border-left-0 border-right-0 border-top-0">Empty list</li>
             }
           </ul>
           <span className="ml-2 text-muted">{state.length} items left</span>
